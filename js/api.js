36702257d086cd4a88da5fa3d6899e2fb1fcef6a
Gcuore <165587875+Gcuore@users.noreply.github.com>
2025-01-25 19:46:53 +0100

    Create api.js

---
+++ js/api.js
@@ -0,0 +1,7 @@
+// Removed all API-related functions for the Websim profile
+// This file is now essentially empty, but kept for potential future use
+
+export async function fetchUserProfile(username) {
+  // Stub function, could be used for future implementation
+  return { username };
+}
